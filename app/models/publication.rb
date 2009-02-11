a5500255d3a71a10fcae4ad7456d8055c75f2330
François Beausoleil <francois@teksol.info>
2009-02-11 10:29:03 -0500

    Attempting to create publication relationships

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,3 +1,9 @@
 class Publication < ActiveRecord::Base
-  has_many :citations, :as => :referrer
+  # "The Rails Way" refers to the BackgrounDRb plugin
+  has_many :references, :as => :referrer, :class_name => "Citation"
+  has_many :referenced, :through => :references, :source => :referrer
+
+  # The BackgrounDRb plugin is referenced from "The Rails Way"
+  has_many :referrals, :as => :referral, :class_name => "Citation"
+  has_many :citations, :through => :referrals, :source => :referral
 end
--- test/unit/publication_test.rb
+++ test/unit/publication_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class PublicationTest < ActiveSupport::TestCase
-  should_have_many :citations
+  context "book references article" do
+    setup do
+      publications(:the_rails_way).referenced << publications(:backgroundrb)
+    end
+
+    should "have the book in the article's citations" do
+      assert_equal [publications(:the_rails_way)], publications(:backgroundrb).citations
+    end
+  end
 end
